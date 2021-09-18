4892a4308ee9d82a110d59814e6197a55c86f6e7
Chandler DeLoach <deloach.cdd@gmail.com>
2021-09-17 19:47:48 -0600

    up the frames per "cycle" to slow light rotation down a bit

--- shader_triangle/res/shaders/vertex.glsl
+++ shader_triangle/res/shaders/vertex.glsl
@@ -16,7 +16,7 @@ void main() {
 	float src_mag = 0.5f * sin(theta);
 
 	vec2 red_src = vec2(0.0f, src_mag) * frame_rot_mat + 0.3f;
-	vec2 blue_src = vec2(-src_mag, -0.5f) * frame_rot_mat + 0.1f;
+	vec2 blue_src = vec2(-src_mag, -src_mag) * frame_rot_mat + 0.1f;
 	vec2 green_src = vec2(src_mag, -0.5f) * frame_rot_mat + 0.2f;
 	vec2 white_src = vec2(-0.0f, -0.75f);
 
